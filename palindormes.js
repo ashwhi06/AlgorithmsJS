e7e4186952870a5155f00c6e7c30c1ea7eaa1c38
Ashe <ashe.white06@gmail.com>
2017-11-03 09:39:50 -0400

    Update palindormes.js

--- palindormes.js
+++ palindormes.js
@@ -1,5 +1,5 @@
 function palindrome (str) 
 {
-	return str = str.split('').reverse().join(''); //split the string into substrings, reverse the array, then join all the elements
+	return str = str.toLowerCase().split('').reverse().join(''); //set all values to lowercase, split the string into substrings, reverse the array, then join all the elements
 }
 
